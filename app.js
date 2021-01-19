4c253dcdaf2f99b693f774a5e232cc3c1385f30a
Sikandar Ali <sikandarali94@hotmail.com>
2021-01-19 14:54:25 +1100

    finished sec2 lec14 interpolation and data binding

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			/* With interpolation we can even output an array or an object for example. It is quite flexible. */
 			courseGoal: 'Finish the course and learn Vue!'
 		};
 	}
