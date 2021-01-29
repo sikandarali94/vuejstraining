5791b3a0ffd9983b9f8b1de145fa06be592ade22
Sikandar Ali <sikandarali94@hotmail.com>
2021-01-30 08:48:00 +1100

    finished sec2 lec15 binding attributes with the v-bind directive

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
 	data() {
 		return {
 			/* With interpolation we can even output an array or an object for example. It is quite flexible. */
-			courseGoal: 'Finish the course and learn Vue!'
+			courseGoal: 'Finish the course and learn Vue!',
+			vueLink: 'https://vuejs.org/'
 		};
 	}
 });
